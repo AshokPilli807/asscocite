4c1d7738505cb550ce7522b61116530a384e72b1
AshokPilli807 <85682487+AshokPilli807@users.noreply.github.com>
2024-03-04 11:23:57 +0530

    newadd

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     github = {
-      source  = "integrations/github"
+      source  = "terraform"
       version = "~> 5.0"
     }
   }
@@ -12,7 +12,7 @@ provider "github" {
     token = "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"
 }
 
-# Add a user to the organization
-resource "github_membership" "membership_for_user_x" {
-  # ...
-}
+# # Add a user to the organization
+# resource "github_membership" "membership_for_user_x" {
+#   # ...
+# }
